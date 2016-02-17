5725d373c33df725f05bba93211ac0b403ddcf93
Aspasibu <andrey.spasibukhov@yandex.ru>
2016-02-17 16:43:26 +0300

    added skeleton of web mvc application without any xml

---
+++ src/main/java/org/aspasibu/springmvc/config/AppConfig.java
@@ -0,0 +1,10 @@
+package org.aspasibu.springmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.aspasibu.springmvc")
+public class AppConfig {
+
+}
