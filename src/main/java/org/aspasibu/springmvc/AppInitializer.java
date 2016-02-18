0309bfc303abc49c548d76ca08ba5328fac44065
aspasibu <andrei.spasibukhov@t-systems.com>
2016-02-18 13:05:48 +0300

    fix jsp compiling problem

--- src/main/java/org/aspasibu/springmvc/AppInitializer.java
+++ src/main/java/org/aspasibu/springmvc/AppInitializer.java
@@ -19,7 +19,7 @@ public class AppInitializer implements WebApplicationInitializer {
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet",
 				new DispatcherServlet(context));
 		dispatcher.setLoadOnStartup(1);
-		dispatcher.addMapping("/*");
+		dispatcher.addMapping("/");
 
 	}
 
